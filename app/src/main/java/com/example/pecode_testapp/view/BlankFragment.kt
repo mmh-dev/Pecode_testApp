8cb07be05c5f7d15f315e5d006c9b2bb9842e607
mmh-dev <murod.hodjaev@gmail.com>
2021-06-03 16:03:39 +0600

    2

--- app/src/main/java/com/example/pecode_testapp/view/BlankFragment.kt
+++ app/src/main/java/com/example/pecode_testapp/view/BlankFragment.kt
@@ -7,21 +7,22 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RemoteViews
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.pecode_testapp.MainActivity
 import com.example.pecode_testapp.R
 import com.example.pecode_testapp.data.FragmentList
 
-class FragmentHome : Fragment() {
+
+class BlankFragment : Fragment() {
 
     private lateinit var addBtn: AppCompatButton
     private lateinit var deleteBtn: AppCompatButton
@@ -42,11 +43,8 @@ class FragmentHome : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        deleteBtn = view.findViewById(R.id.minus_btn)
 
-        if (FragmentList.fragments.size == 1){
-            deleteBtn.visibility = View.INVISIBLE
-        }
+        deleteBtn = view.findViewById(R.id.minus_btn)
 
         pageNumber = view.findViewById(R.id.page_number)
         pageNumber.text = FragmentList.fragments.size.toString()
@@ -54,7 +52,7 @@ class FragmentHome : Fragment() {
         addBtn = view.findViewById(R.id.plus_btn)
 
         addBtn.setOnClickListener {
-            val fragment = BlankFragment()
+            val fragment = FragmentHome()
             FragmentList.add(fragment)
             parentFragmentManager.beginTransaction().apply {
                 replace(R.id.viewPager, fragment)
@@ -62,45 +60,37 @@ class FragmentHome : Fragment() {
             }
             FragmentList.update()
             FragmentList.switchToPage(FragmentList.fragments.size - 1)
+            Log.i("TAG", FragmentList.fragments.size.toString())
         }
         deleteBtn.setOnClickListener {
 
         }
 
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         val notificationLayout  = RemoteViews(context?.packageName, R.layout.notif_layout)
         notificationLayout.setTextViewText(R.id.notification_title, "Notification " + FragmentList.fragments.size)
         notificationLayout.setTextViewText(R.id.notification_text, "You create a notification")
 
-        notificationManager = getSystemService(requireContext(), NotificationManager::class.java) as NotificationManager
+        notificationManager = ContextCompat.getSystemService(
+            requireContext(),
+            NotificationManager::class.java
+        ) as NotificationManager
         notificationBtn = view.findViewById(R.id.notification_btn)
         notificationBtn.setOnClickListener {
+            notificationChannel = NotificationChannel(channelId, desc, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
 
-            val intent = Intent(requireContext(), MainActivity::class.java)
-            val pendingIntent = PendingIntent.getActivity(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            builder = Notification.Builder(requireContext(), channelId)
+                .setContent(notificationLayout)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.notif_icon))
+                .setSmallIcon(R.drawable.notif_icon)
+                .setContentIntent(pendingIntent)
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-
-                notificationChannel = NotificationChannel(channelId, desc, NotificationManager.IMPORTANCE_HIGH)
-                notificationChannel.enableLights(true)
-                notificationChannel.lightColor = Color.GREEN
-                notificationChannel.enableVibration(false)
-                notificationManager.createNotificationChannel(notificationChannel)
-
-                builder = Notification.Builder(requireContext(), channelId)
-                    .setContent(notificationLayout)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.notif_icon))
-                    .setSmallIcon(R.drawable.notif_icon)
-                    .setContentIntent(pendingIntent)
-            }
-            else{
-
-                builder = Notification.Builder(requireContext())
-                    .setContent(notificationLayout)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.notif_icon))
-                    .setSmallIcon(R.drawable.notif_icon)
-                    .setContentIntent(pendingIntent)
-            }
-            notificationManager.notify(FragmentList.fragments.size, builder.build())
+            notificationManager.notify(2, builder.build())
         }
     }
 }
